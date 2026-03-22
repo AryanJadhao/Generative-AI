5f789bf08f9ab64e3a42aa1deabb67b4a5973369
AryanJadhao <aryanjadhao29@gmail.com>
2026-03-23 00:06:34 +0530

    Adding Document Loaders

---
+++ RAG-Document_Loaders/PyPDFLoader.py
@@ -0,0 +1,11 @@
+# pip install pypdf
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs = loader.load()
+
+print(len(docs))
+
+print(docs[0].page_content)
+print(docs[1].metadata)
